1f94d3f7a4d9d564dad3e7093637ad5c8bac2885
Zeze812-dev <lublumarmelad23@mail.ru>
2025-03-18 04:14:43 +0300

    Update page.js

--- app/catalog/gloves/page.js
+++ app/catalog/gloves/page.js
@@ -1,13 +1,12 @@
-import hoodie_img1 from "@/assets/items/hoodie/hoodie1.webp"
-import hoodie_img2 from "@/assets/items/hoodie/hoodie2.webp"
-import hoodie_img3 from "@/assets/items/hoodie/hoodie3.webp"
-import hoodie_img4 from "@/assets/items/hoodie/hoodie4.webp"
-import hoodie_img5 from "@/assets/items/hoodie/hoodie5.webp"
+import hoodie_img1 from "@/assets/items/gloves/gloves1.webp"
+import hoodie_img2 from "@/assets/items/gloves/gloves2.webp"
+import hoodie_img3 from "@/assets/items/gloves/gloves3.webp"
+import hoodie_img4 from "@/assets/items/gloves/gloves4.webp"
+import hoodie_img5 from "@/assets/items/gloves/gloves5.webp"
 import hoodie_img6 from "@/assets/items/hoodie/hoodie6.webp"
-import hoodie_img7 from "@/assets/items/hoodie/hoodie7.webp"
-import hoodie_img8 from "@/assets/items/hoodie/hoodie8.webp"
-import hoodie_img9 from "@/assets/items/hoodie/hoodie9.webp"
-
+import hoodie_img7 from "@/assets/items/gloves/gloves7.webp"
+import hoodie_img8 from "@/assets/items/gloves/gloves8.webp"
+import gloves from "@/assets/catalog/gloves.webp"
 import girl from "@/assets/items/gloves/gloves_girl.webp"
 
 import {ItemPage} from "@/components/items/item_page";
@@ -15,12 +14,13 @@ import Image from "next/image";
 
 export default function CatalogPage() {
     const size = ["Phantom Color"];
-    const imgs = [hoodie_img1, hoodie_img2, hoodie_img3, hoodie_img4, hoodie_img5, hoodie_img6, hoodie_img7, hoodie_img8, hoodie_img9]
+    const imgs = [hoodie_img1, hoodie_img2, hoodie_img3, hoodie_img4, hoodie_img5, hoodie_img6, hoodie_img7, hoodie_img8,]
+    const png=[gloves]
 
     return (
         <>
             <title>Muiraje Hoodie</title>
-            <ItemPage size={size} imgs={imgs} title="Catalog/Hoodie" itemName="COZY BLUE GLOVES UNISEX"
+            <ItemPage size={size} png={png} imgs={imgs} title="Catalog/Hoodie" itemName="COZY BLUE GLOVES UNISEX"
                       about1="Без ожиданий. В наличии. Отправка в течение 2-3 дней.
 "
                       about2="Я –  вязанные митенки голубого цвета. Идеально подойду для холодной зимы. У меня откидывающаяся верхняя часть, что позволяет держать пальцы открытыми, когда это необходимо – например, чтобы сделать снимок на Nokia или взять чашку горячего кофе с зефирками. На мне изображён рисунок – портрет девушки, воплощение загадочности.
