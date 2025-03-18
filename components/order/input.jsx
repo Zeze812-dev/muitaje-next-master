7c03d7d6491daafcb15108bb82e0c5730899a2a5
Zeze812-dev <lublumarmelad23@mail.ru>
2025-03-18 04:19:43 +0300

    Update input.jsx

--- components/order/input.jsx
+++ components/order/input.jsx
@@ -1,6 +1,6 @@
 export default function Input({children, name="", type = "text", required=false}) {
     return (
-        <div className="w-full">
+        <div className="w-3/5">
             <h3>{children}</h3>
             <input required={required} type={type} name={name} className="w-full bg-stone-300 border-l-2 border-t-2 border-stone-400 hover:outline-0"/>
         </div>
