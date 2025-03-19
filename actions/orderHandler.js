c0b7e251e7b27c17c08f099c93880740e1b8364b
Zeze812-dev <lublumarmelad23@mail.ru>
2025-03-20 02:06:37 +0300

    Create orderHandler.js

---
+++ actions/orderHandler.js
@@ -0,0 +1,26 @@
+// orderHandler.js
+'use server'
+
+import nodemailer from 'nodemailer';
+
+export async function orderHandler(formData, cart) {
+    console.log(formData, cart);
+
+    let transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: 'kambulatfanboy@gmail.com',
+            pass: 'pvurpoycibimpgdi',
+        },
+    });
+
+    await transporter.sendMail({
+        from: '"MUIRAJE" <kambulatfanboy@gmail.com>',
+        to: formData.get('email'),
+        subject: 'YOU ORDER FROM MUIRAJE',
+        text: 'YOU ORDER FROM MUIRAJE',
+        html: 'This <i>message</i> with <strong>attachments</strong>.',
+    });
+
+    console.log('Email sent successfully');
+}
