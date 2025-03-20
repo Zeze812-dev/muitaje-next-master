8fd4b3a53c57a38865de8765f43b7c28e5274491
Zeze812-dev <lublumarmelad23@mail.ru>
2025-03-20 08:15:23 +0300

    Update page.js

--- app/catalog/Gray_Top/page.js
+++ app/catalog/Gray_Top/page.js
@@ -15,14 +15,15 @@ import Image from "next/image";
 export default function TopPage() {
     const size = ["S", "M", "L", "XL"];
     const color = ["White_Top", "Gray_Top"];
+    const displayColor = color.map(item => item.replace(/_/g, ' '));
     const imgs = [top_img1, top_img2, top_img3, top_img4, top_img5]
     const png = [topgray]
     return (
         <>
             <title>Muiraje Top</title>
-            <ItemPage size={size} png={png} color={color} imgs={imgs} title="Catalog/Top" itemName="COZY GRAY TANK TOP UNISEX"
+            <ItemPage size={size} png={png} color={color} displayColor={displayColor} imgs={imgs} title="Catalog/Top" itemName="COZY GRAY TANK TOP UNISEX"
                       about1="Отправка в течение 7 дней. Изготавливаюсь непосредственно в Санкт-Петербурге :)"
-                      about2="Легко сочетаюсь с элементами гардероба, помогая создавать разнообразные образы.(Вы можете выбрать длину майки из списка или указать желаемую длину в комментарии при оформлении заказа). Хлопок 100%. "
+                      about2="Легко сочетаюсь с элементами гардероба, помогая создавать разнообразные образы. (Вы можете выбрать длину майки из списка или указать желаемую длину в комментарии при оформлении заказа). Хлопок 100%. "
                       price={2624}>
                 <Image unoptimized width={350} height={1000} src={girl} alt="" className="absolute bottom-36 right-20  "/>
                 {/* <img src={girl} alt="" className="md:static z-10 w-52 absolute bottom-0 right-0 " /> */}
