cdce5fc6654313ffd4a928b4ca71500e5249bb57
Zeze812-dev <lublumarmelad23@mail.ru>
2025-03-22 03:28:23 +0300

    Update page.js

--- app/catalog/Black/page.js
+++ app/catalog/Black/page.js
@@ -11,7 +11,7 @@ import lighter_img9 from "@/assets/items/blackwhitelighter/firebw9.webp"
 
 import girl from "@/assets/items/lighter/lighter-girl.webp"
 
-import {ItemPage} from "@/components/items/item_page";
+import {ItemPage} from "@/components/items/item_page2";
 
 import React from 'react';
 import Image from "next/image";
@@ -20,12 +20,13 @@ import Image from "next/image";
 export default function LighterPage() {
     const size = ["Black", "White"];
     const color = ["Blue", "Red"];
+    const displayColor = color.map(item => item.replace(/_/g, ' '));
     const imgs = [lighter_img1, lighter_img2, lighter_img3, lighter_img4, lighter_img5, lighter_img6, lighter_img7, lighter_img8, lighter_img9]
     const png = [lighter_png]
     return (
         <>
             <title>Muiraje Lighter</title>
-            <ItemPage png={png} size={size} color={color} imgs={imgs} title="Catalog/Ligther" itemName="COZY BLACK AND WHITE LIGHTERS"
+            <ItemPage png={png} size={size} color={color} displayColor={displayColor}  imgs={imgs} title="Catalog/Ligther" itemName="COZY BLACK AND WHITE LIGHTERS"
                       about1="Без ожиданий. В наличии. Доставка в любой день."
                       about2="Зажигалка - это не просто инструмент, а незаменимый спутник в любой ситуации, будь то романтический вечер при свечах, поход в горы или дружеская встреча на природе. А кастомизированные зажигалки подчеркивают индивидуальность владельца, превращаясь в стильный аксессуар, уникальный подарок и символ особенного отношения к деталям.
 Матовое покрытие.
