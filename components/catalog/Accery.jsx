e321035a4f51b13c37ef5eba5f212d25029b0c0e
Zeze812-dev <lublumarmelad23@mail.ru>
2025-03-26 02:41:15 +0300

    Update Accery.jsx

--- components/catalog/Accery.jsx
+++ components/catalog/Accery.jsx
@@ -53,7 +53,7 @@ export function Catalog() {
                 <div className=" border-l-2 heightother border-t-2 border-stone-400">
                     <div className="border-r-2 heightother border-b-2 border-stone-300">
                         <main className="border-l-2 border-t-2 heightother border-stone-600 p-4 grid-cols-2 grid md:grid-cols-3 xl:grid-cols-4">
-                            <Item image={perc} hoverImage={perc2} page="gloves" name="COZY BLUE GLOVES UNISEX" price="2924" />
+                            <Item image={perc} hoverImage={perc2} page="gloves" name="OZY MULTI-COLORED LIGHTERS" price="2924" />
                             <Item image={podv} hoverImage={podv} page="necklace" name="COZY SILVER NECKLACE UNISEX" price="8424" />
                             <Item image ={fire} hoverImage={fireHover} page="Black" name="COZY BLUE AND RED LIGHTERS" price="224"/>
                         </main>
