3383c84d042aaa62364d3480859084711d0acfcb
Zeze812-dev <lublumarmelad23@mail.ru>
2025-03-26 02:40:57 +0300

    Update Catalog.jsx

--- components/catalog/Catalog.jsx
+++ components/catalog/Catalog.jsx
@@ -22,11 +22,11 @@ function Item({ name, price, page, image, hoverImage }) {
     const [isHovered, setIsHovered] = useState(false);
 
     const handleMouseOver = () => {
-        setIsHovered(true); // Меняем состояние при наведении
+        setIsHovered(true);
     };
 
     const handleMouseOut = () => {
-        setIsHovered(false); // Возвращаем исходное состояние при уходе мыши
+        setIsHovered(false);
     };
     return (
         <Link className="hover:bg-stone-100 m-2 leading-3" href={"/catalog/" + page}>
@@ -51,13 +51,13 @@ export function Catalog() {
         <>
         
             <div className="mx-3 mb-3 bg-white border-r-2 border-b-2 border-stone-100 overflow-auto flex-1">
-                <div className=" border-l-2 border-t-2 border-stone-400">
+                <div className=" border-l-2  border-t-2 border-stone-400">
                     <div className="border-r-2 border-b-2 border-stone-300">
                         <main className="border-l-2 border-t-2 border-stone-600 p-4 grid-cols-2 grid md:grid-cols-3 xl:grid-cols-4">
                             <Item image={hoodie} hoverImage={hoodie} page="hoodie" name="COZY OVERSIZE GRAY HOODIE UNISEX" price="4824" />
                             <Item image={pants} hoverImage={pantsHover} page="Light_Gray" name="COZY OVERSIZE GRAY PANTS UNISEX" price="5824" />
-                            <Item image={top} hoverImage={topHover} page="White_Top" name="COZY GRAY TANK TOP UNISEX" price="2624" />
-                            <Item image={perc} hoverImage={perc2} page="gloves" name="COZY BLUE GLOVES UNISEX" price="2924" />
+                            <Item image={top} hoverImage={topHover} page="White_Top" name="COZY TANK TOPS UNISEX" price="2624" />
+                            <Item image={perc} hoverImage={perc2} page="gloves" name="OZY MULTI-COLORED LIGHTERS" price="2924" />
                             <Item image={podv} hoverImage={podv2} page="necklace" name="COZY SILVER NECKLACE UNISEX" price="8424" />
                             <Item image ={fire} hoverImage={fireHover} page="Black" name="COZY BLUE AND RED LIGHTERS" price="224"/>
                         </main>
