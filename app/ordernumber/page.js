7208182a0b632426a7bbf19dc6f7bf34ddaa561c
Zeze812-dev <lublumarmelad23@mail.ru>
2025-04-06 19:46:40 +0300

    Add files via upload

--- app/ordernumber/page.js
+++ app/ordernumber/page.js
@@ -1,6 +1,7 @@
 "use client"
 import React, { useState, useEffect } from 'react';
 import './OrderNumber.css';
+import logo from "@/assets/logoorder.webp"
 import itworked2 from '@/components-assets/order/itworked23.webp';
 import soon from '@/components-assets/order/soon.webp';
 import { Window, WindowBorder } from '@/components/documents/window2.jsx';
@@ -27,21 +28,22 @@ const OrderNumber = () => {
             <title>Order Number</title>
 
             <div className="block">
-                <Window title="Your order" bg_bar="grey">
+                <Window title="Your order" bg_bar="grey" logo={logo}>
                     <main className="block justify-center text-center align-middle">
                         <div className="block w-2/4 m-auto align-middle justify-center">
-                            <Image className="ordernumberw ordernumberh mx-auto" src={itworked2} alt="" />
+                            <h2 className="  ordercolor txtorderphone text-justify fontsizetxtorder lineheightordernumberphone2 ">It Worked, yay!</h2>
+                                <h2 className="ordercolor txtorderphone text-justify fontsizetxtorder lineheightordernumberphone lineheightordernumberphone2 ">Here's your order number:</h2>
                         </div>
                         <div className="w-full flex justify-center text-center">
                             <h2 className="text-8xl caret-colortxtcart2 pt-20 pb-20 justify-center content-center">{orderNumber}</h2>
                         </div>
                         <div className="block w-2/4 m-auto align-middle justify-center">
-                            <Image className="h-40 mx-auto render" src={soon} alt="" />
-                            <a href="https://t.me/care224radge" target="_blank" rel="noopener noreferrer">
+                            <h2 className=" ordercolor text-justify txtorderphone fontsizetxtorder lineheightordernumberphone lineheightordernumberphone2 ">Скоро с вами свяжется Care для уточнения деталей в Telegram! :)</h2>
+
                                 <div className="flex pt-5 pb-0 justify-center content-center">
-                                    <Image className="h-10 w-72 render" src={tg} alt="" />
+                                    <a href="https://t.me/care224radge" target="_blank" rel="noopener noreferrer" className="ordercolor ordertgfont  text-justify fontsizetxtorder ">https://t.me/care224radge</a>
                                 </div>
-                            </a>
+
                         </div>
                     </main>
                 </Window>
